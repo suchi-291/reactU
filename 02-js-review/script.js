71a7cf371591a70a2c7a3da55264377f6d43e880
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-07-21 15:24:26 +0530

    js review

--- 02-js-review/script.js
+++ 02-js-review/script.js
@@ -142,3 +142,123 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+
+/*
+// Since arrays and objects are reference data types, We do not want to tamper with the data inside the data array, hence we are creating a new array called books to practice
+
+// DESTRUCTURING
+
+const books = getBooks();
+//console.log(books);
+//books;
+
+const book = getBook(2);
+console.log(book);
+//const title = book.title;
+//title;
+const { id, title, genres, ...myObj } = book;
+
+id;
+title;
+myObj;
+genres;
+
+const newGenres = [genres, "romance"];
+newGenres; //This is not how we want so at times as such we can use spread operator
+
+const newGenres2 = [...genres, "romance"];
+newGenres2;
+
+const updatedBook = { ...book, moviePublicationDate: "2001-12-19" };
+console.log(updatedBook);
+*/
+
+// lets pretend that we are getting books from the fictional API using the getBooks() function
+
+const books = getBooks();
+
+//const x = [1, 2, 3, 4].map((el) => el * 2);
+//console.log(x);
+
+//const titles = books.map((book) => book.title);
+//titles;
+
+/*const essentialDataDummy = books.map((book) => {
+  book.title, book.author;
+}); // if we use the {} braces in side the function, the function will think it is a declaration and not an object.
+essentialDataDummy;*/
+
+/*const essentialData = books.map((book) => ({
+  title: book.title,
+  author: book.author,
+}));
+
+essentialData;*/
+
+/*const longBooksWithMovie = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+
+longBooksWithMovie;
+
+const adventureBooks = books
+  .filter((book) => book.genres.includes("adventure"))
+  .map((book) => book.title);
+adventureBooks;*/
+
+/*const pagesAllBooks = books.reduce((acc, book) => acc + book.pages, 0);
+pagesAllBooks;
+*/
+
+/*const x = [3, 7, 1, 9, 6];
+const sorted = x.slice().sort((a, b) => a - b);
+
+sorted;
+
+const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages);
+sortedByPages;
+*/
+
+/************************************************************/
+/*  WORKING WITH IMMUTABLE ARRAYS */
+
+// 1) ADDING BOOK OBJECT TO ARRAY
+
+/*const newBook = {
+  id: 6,
+  title: "Harry Potter and the Chamber of Secrets",
+  author: "J.K. Rowling",
+};
+
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+// 2) DELETE BOOK OBJECT FROM THE ARRAY
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id !== 3);
+booksAfterDelete;
+
+// 3) UPDATE BOOK OBJECT IN THE ARRAY
+const booksAfterUpdate = booksAfterDelete.map((book) =>
+  book.id === 1 ? { ...book, pages: 1240 } : book
+);
+booksAfterUpdate;
+*/
+/******************************************************/
+/* ASYNCHRONOUS JAVASCRIPT */
+
+// PROMISES
+
+/*fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+*/
+
+// ASYNC
+
+async function getTodos() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await response.json();
+  console.log(data);
+}
+
+getTodos();
